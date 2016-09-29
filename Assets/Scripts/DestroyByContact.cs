74568c3f923d0f2dfc587119d96b9af391b1600b
jvhgamer <jackhiggins2017@rohrsci-mc-08.local>
2016-09-29 15:59:00 -0700

    Initial commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary")	//prevent asteroid from being destroyed from just being in the boundary
+			return;	//ends execution of function
+
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") 
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+
+		gameController.addScore (scoreValue);
+
+		Destroy (other.gameObject);	//destroy bolt
+		Destroy (gameObject);		//destroy astroid
+	}
+}
